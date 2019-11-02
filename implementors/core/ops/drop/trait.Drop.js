5d8fe73ed1c16768c1b2461c8bdc721e666036aa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-02 19:12:49 +0000

    Deploy Smithay/wayland-rs to github.com/Smithay/wayland-rs.git:gh-pages

---
+++ implementors/core/ops/drop/trait.Drop.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors["wayland_client"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wayland_client/struct.ReadEventsGuard.html\" title=\"struct wayland_client::ReadEventsGuard\">ReadEventsGuard</a>",synthetic:false,types:["wayland_client::event_queue::ReadEventsGuard"]},];
+implementors["wayland_commons"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wayland_commons/socket/struct.Socket.html\" title=\"struct wayland_commons::socket::Socket\">Socket</a>",synthetic:false,types:["wayland_commons::socket::Socket"]},];
+implementors["wayland_cursor"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wayland_cursor/struct.CursorTheme.html\" title=\"struct wayland_cursor::CursorTheme\">CursorTheme</a>",synthetic:false,types:["wayland_cursor::CursorTheme"]},];
+implementors["wayland_egl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"wayland_egl/struct.WlEglSurface.html\" title=\"struct wayland_egl::WlEglSurface\">WlEglSurface</a>",synthetic:false,types:["wayland_egl::WlEglSurface"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
